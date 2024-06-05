79e1babf4f2708f22cf3a3210f00aed1475f68e3
Pranav Padmanabhan <120774309+Pranav9605@users.noreply.github.com>
2024-06-05 09:53:25 +0530

    Time: 59 ms (17.34%), Space: 16.4 MB (98.88%) - LeetHub

---
+++ 3226-minimum-number-game/3226-minimum-number-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        
+        nums.sort()
+        for x in range(0,len(nums),2):
+            nums[x],nums[x+1]=nums[x+1],nums[x]
+            
+        return nums
+
+        
+
+
+
+        
+        
