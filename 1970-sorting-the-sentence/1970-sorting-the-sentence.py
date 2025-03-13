ee0b4ccc5d419287ae626b3470129d488022e507
Pranav Padmanabhan <120774309+Pranav9605@users.noreply.github.com>
2025-03-13 11:14:43 +0530

    Time: 40 ms (41.14%), Space: 17.5 MB (90.83%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.py
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.py
@@ -1,14 +1,37 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         a=s.split()
-        b=''
-        c=[]
-        d=(sorted(a,key=lambda x:x[-1]))
-        print(d)
-        for x in d:
-            c.append(x[:-1])
-        print(c)
-        b=' '.join(c)
-        print(b)
-        return b
+        a=sorted(a,key=lambda x:x[-1])
+        out=[]
+        for x in a:
+            out.append(x[:-1])
+        out=" ".join(out)
+        return out    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # a=s.split()
+        # b=''
+        # c=[]
+        # d=(sorted(a,key=lambda x:x[-1]))
+        # print(d)
+        # for x in d:
+        #     c.append(x[:-1])
+        # print(c)
+        # b=' '.join(c)
+        # print(b)
+        # return b
         
