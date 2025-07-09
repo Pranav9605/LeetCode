5f10c5673d52f0e9b7900deb1bd4f123aac0d22c
Pranav Padmanabhan <120774309+Pranav9605@users.noreply.github.com>
2025-07-09 16:52:48 +0530

    Time: 570 ms (71.46%), Space: 26.7 MB (18.56%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,26 +1,52 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        result=[]
-        def merge(nums):
-            if len(nums)==1:
+        def quick(nums):
+            if len(nums)<=1:
                 return nums
-            mid=len(nums)//2
-            left=merge(nums[:mid])
-            right=merge(nums[mid:])
-            return sort(left,right)
-        def sort(left,right):
-            l=r=0
-            result=[]
-            while l<len(left) and r<len(right):
-                if left[l]<right[r]:
-                    result.append(left[l])
-                    l+=1
-                else:
-                    result.append(right[r])
-                    r+=1
-            result.extend(left[l:])
-            result.extend(right[r:])
-            return result
-        a=merge(nums)
-        return a
+            pivot=random.choice(nums)
+            left=[x for x in nums if x<pivot]
+            middle = [x for x in nums if x == pivot]
+            right=[x for x in nums if x>pivot]
+            return quick(left)+middle+quick(right)
+        return quick(nums)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # def merge(nums):
+        #     if len(nums)==1:
+        #         return nums
+        #     mid=len(nums)//2
+        #     left=merge(nums[:mid])
+        #     right=merge(nums[mid:])
+        #     return sort(left,right)
+        # def sort(left,right):
+        #     l=r=0
+        #     result=[]
+        #     while l<len(left) and r<len(right):
+        #         if left[l]<right[r]:
+        #             result.append(left[l])
+        #             l+=1
+        #         else:
+        #             result.append(right[r])
+        #             r+=1
+        #     result.extend(left[l:])
+        #     result.extend(right[r:])
+        #     return result
+        # a=merge(nums)
+        # return a
         
